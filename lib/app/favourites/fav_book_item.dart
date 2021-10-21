40b4c62ea5b6773e6a295ceabc60075f08686d4e
ctechdev <tassonecosimo@gmail.com>
2021-10-21 13:56:58 +0200

    fix fav

---
+++ lib/app/favourites/fav_book_item.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class FavBookItem extends StatelessWidget {
+  final Map<String, dynamic> data;
+  const FavBookItem({required this.data, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      elevation: 5,
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: data['imageLinks'] != null
+                  ? SizedBox(
+                      height: 180,
+                      width: 120,
+                      child: Image.network(
+                        data['imageLinks'],
+                        fit: BoxFit.fill,
+                      ),
+                    )
+                  : SizedBox(
+                      height: 180,
+                      width: 120,
+                      child: Image.asset('assets/no_cover.gif',
+                          fit: BoxFit.fill, semanticLabel: 'No Cover'),
+                    ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              // mainAxisSize: MainAxisSize.min,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SizedBox(
+                  width: 180,
+                  child: data['authors'] != null
+                      ? Text(
+                          'by ${data['authors'][0]}',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey.withOpacity(0.70)),
+                        )
+                      : Text(
+                          'by Nd',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey.withOpacity(0.70)),
+                        ),
+                ),
+                const SizedBox(height: 6),
+                SizedBox(
+                  width: 190,
+                  child: data['title'] != null
+                      ? Text(data['title'] as String,
+                          maxLines: 2,
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold))
+                      : const Text('Nd',
+                          maxLines: 2,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
+                ),
+                const SizedBox(height: 6),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star, color: Colors.yellow[600]),
+                    const SizedBox(width: 4),
+                    Text(data['averageRating'].toString()),
+                  ],
+                ),
+                const SizedBox(height: 6),
+                // Chip(
+                //   padding: EdgeInsets.zero,
+                //   backgroundColor: Colors.blue[200],
+                //   label:  data['categories'] != null
+                //         ? Text(data['categories'].first,
+                //             style: const TextStyle(fontSize: 12))
+                //         : const Text('Uncategorized'),
+                // )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/app/favourites/fav_page.dart
+++ lib/app/favourites/fav_page.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_books_test/app/favourites/fav_book_item.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class FavouritesPage extends HookConsumerWidget {
@@ -7,33 +10,62 @@ class FavouritesPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
+    final firestoreInstance = FirebaseFirestore.instance;
+    var currentUser = FirebaseAuth.instance.currentUser;
+
+    final Stream<QuerySnapshot> _booksStream = firestoreInstance
+        .collection('users')
+        .doc(currentUser!.uid)
+        .collection('books')
+        .snapshots();
 
     return SafeArea(
       child: Scaffold(
           body: Stack(
-            children: const <Widget>[
-              Positioned(
-                  top: 30,
-                  left: 20,
-                  child: SizedBox(
-                      width: 260,
-                      child: Text('Your favourite Books',
-                          style: TextStyle(
-                            //color: Color(0xffC4C6CC),
-                            fontSize: 26.0,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Brutal',
-                          ),
-                          maxLines: 2))),
+        children: <Widget>[
+          const Positioned(
+              top: 30,
+              left: 20,
+              child: SizedBox(
+                  width: 260,
+                  child: Text('Your favourite Books',
+                      style: TextStyle(
+                        //color: Color(0xffC4C6CC),
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Brutal',
+                      ),
+                      maxLines: 2))),
+          Positioned(
+              top: 80,
+              left: 20,
+              right: 20,
+              bottom: 4,
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _booksStream,
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Something went wrong');
+                  }
 
-              // Positioned(
-              //   top:185,
-              //   left: 20,
-              //   right:20,
-              //   bottom: 4,
-              //   child: Text('Favs')),
-            ],
-          )),
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text("Loading");
+                  }
+
+                  return ListView(
+                    children:
+                        snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data =
+                          document.data()! as Map<String, dynamic>;
+                      print(document.data().toString());
+                      return FavBookItem(data: data);
+                    }).toList(),
+                  );
+                },
+              )),
+        ],
+      )),
     );
   }
 }
